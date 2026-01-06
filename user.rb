40b9f902282c7c8da8ab69d72b0d0b395eb99ffa
Baha Amayreh <amayreh.1@osu.edu>
2026-01-06 13:48:34 -0500

    update the user model

--- user.rb
+++ user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
     has_many :posts , dependent: :destroy
+    has_one :address, dependent: :destroy
     before_save { self.email = email.downcase }
-    validates :name, presence:true, length:{minimum: 4, maximum: 50}
-    validates :email, presence:true, length:{ maximum: 100}, 
-      uniqueness:true,format:{with: /\A(\w+.\d+@osu\.edu)|(\w+@gmail\.com)\Z/i }
-    validates :password, presence:true, 
-              length:{minimum:6}
+    validates :name, presence:{message:'Name is required'}, length:{minimum: 5, maximum: 50}
+    validates :email, presence:true, length:{ minimum:10, maximum: 100}, 
+      uniqueness:true,format:{with: /\A([A-Za-z]+\.\d+@osu\.edu)|(\w+@gmail\.com)\Z/i ,
+    message:'Only OSU or Gmail'
+    }
+    validates :password, presence:true, length:{minimum:6, maximum:20}
     has_secure_password
 end
