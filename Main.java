128c115d5a634aec014ad1abb07801fd50efbf19
Baha Amayreh <amayreh.1@osu.edu>
2026-02-12 15:50:22 -0500

    add Main.java

---
+++ Main.java
@@ -0,0 +1,47 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int Sara_mark = 75;
+        int Bob_mark = 88;
+        int David_mark = 77;
+
+        // Sara's Grade
+        if (Sara_mark >= 90) {
+            System.out.println("Grade of Sara = A");
+        } else if (Sara_mark >= 80) {
+            System.out.println("Grade of Sara = B");
+        } else if (Sara_mark >= 70) {
+            System.out.println("Grade of Sara = C");
+        } else if (Sara_mark >= 60) {
+            System.out.println("Grade of Sara = D");
+        } else {
+            System.out.println("Grade of Sara = F");
+        }
+
+        // Bob's Grade
+        if (Bob_mark >= 90) {
+            System.out.println("Grade of Bob = A");
+        } else if (Bob_mark >= 80) {
+            System.out.println("Grade of Bob = B");
+        } else if (Bob_mark >= 70) {
+            System.out.println("Grade of Bob = C");
+        } else if (Bob_mark >= 60) {
+            System.out.println("Grade of Bob = D");
+        } else {
+            System.out.println("Grade of Bob = F");
+        }
+
+        // David's Grade
+        if (David_mark >= 90) {
+            System.out.println("Grade of David = A");
+        } else if (David_mark >= 80) {
+            System.out.println("Grade of David = B");
+        } else if (David_mark >= 70) {
+            System.out.println("Grade of David = C");
+        } else if (David_mark >= 60) {
+            System.out.println("Grade of David = D");
+        } else {
+            System.out.println("Grade of David = F");
+        }
+    }
+}
